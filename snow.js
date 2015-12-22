cc7b4035565289aa339d05b7abcd6841fd78392d
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2015-12-22 18:53:23 -0500

    Animation performance and aesthetic improvements

--- snow.js
+++ snow.js
@@ -1,3 +1,13 @@
 (function(){
-  document.body.appendChild(document.createElement('eager-app-snow'));
+  var snow = document.createElement('eager-app-snow');
+  snow.innerHTML = '<eager-app-snow-a></eager-app-snow-a><eager-app-snow-b></eager-app-snow-b><eager-app-snow-c></eager-app-snow-c>';
+  document.body.appendChild(snow);
+
+  setTimeout(function(){
+    snow.setAttribute('eager-app-snow-fade-out', '');
+  }, 3 * 1000);
+
+  setTimeout(function(){
+    snow.parentNode.removeChild(snow);
+  }, 20 * 1000);
 })();
